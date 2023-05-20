f61ff3d1ab7991f325febe6e0ff9d6cab5fa7a94
afrazier01 <amfrazier01@gmail.com>
2023-05-19 22:21:46 -0500

    added function to generate logo

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const inquirer = require('inquirer')
 const questions = require('./lib/questions')
+const {appendFile} = require('fs')
+const generateSvg = require('./lib//GenerateSVG')
 
 inquirer
     .prompt(questions)
     .then((answers)=>{
     const {text, textColor, shape, shapeColor} = answers;
-    console.log(text,textColor,shape,shapeColor);
+    
+    const logo = generateSvg(text, textColor, shape, shapeColor)
+    console.log(logo)
+    //appendFile('logo.svg',[])
+    console.log('\n'+'Generated logo.svg');
 });
